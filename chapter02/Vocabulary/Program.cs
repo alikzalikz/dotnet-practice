0e1bc5b47d3120df0ad4203ddd824543565c6725
hana zamani <hanazamani96@gmail.com>
2026-06-20 16:32:47 +0330

    edit file

--- chapter02/Vocabulary/Program.cs
+++ chapter02/Vocabulary/Program.cs
@@ -1,6 +1,4 @@
 ﻿string name = "hana zamani";
 int age = 22 ;
 Console.WriteLine ($"{name} is {age}");
-string boyfrindsname = "ali";
-int hisage = 23 ;
-Console.WriteLine ($"his boy frind {boyfrindsname} is {hisage}");
+
