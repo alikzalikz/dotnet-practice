c20f9029b2e1c8831ccabbf687e1b5e316b63a69
hana zamani <hanazamani96@gmail.com>
2026-06-22 19:34:49 +0330

    add vocabulary 7

---
+++ chapter02/vocabulary7/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+string telphoneNumber = "02111223344" ; //A person’s telephone number
+WriteLine($"my telphone number: {telphoneNumber}.");
+double height = 1.68 ; //A person’s height
+WriteLine($"my height is  {height}. ");
+int age = 22 ; //A person’s age
+WriteLine ($"I am {age} years old.");
+decimal salary = 1000m ; //A person’s salary
+WriteLine ($"my salary is {salary :C}.");
+long ISBN = 9786007159644 ; //A book’s ISBN
+WriteLine ($"ISBN of my favorite book is: {ISBN}. ");
+decimal price = 4.11M ; //A book’s price
+WriteLine($"t]The prcie of this book is {price :C}.");
+double weight = 1.23 ; //A book’s shipping weight
+WriteLine($"The weight of my favorite book is {weight} kg. ");
+long population = 90000000 ; //A country’s population
+WriteLine($"my country's population is {population :N0}. ");
+ulong  stars = 123456789 ; //The number of stars in the universe
+WriteLine($"There are {stars} stars in the universe. ");
+int employees = 352;  //The number of employees in each of the small or medium businesses in the Iran
+WriteLine($"{employees} employees work in our company. ");
