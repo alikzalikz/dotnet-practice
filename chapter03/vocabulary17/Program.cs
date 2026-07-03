3d3135e88d070243995de6186725117444b24e78
hana zamani <hanazamani96@gmail.com>
2026-07-03 16:53:31 +0330

    add loops practice

---
+++ chapter03/vocabulary17/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("enter a number:");
+int num= int.Parse(Console.ReadLine()!);
+int x = num;
+while(0 < x)
+{
+    Console.WriteLine(x);
+    x--;
+}
---
+++ chapter03/vocabulary18/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("enter a number:");
+int num= int.Parse(Console.ReadLine()!);
+int x = 1;
+while(x <= num)
+{
+    Console.WriteLine(x);
+    x++;
+}
