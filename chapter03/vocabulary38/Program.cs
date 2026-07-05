1db5c58e0480991d7f4f34dea05811401b0fea20
hana zamani <hanazamani96@gmail.com>
2026-07-05 22:10:55 +0330

    add convert practice

---
+++ chapter03/vocabulary38/Program.cs
@@ -0,0 +1,3 @@
+﻿double number = 12.6;
+int x =Convert.ToInt32(number);
+Console.WriteLine(x);
---
+++ chapter03/vocabulary39/Program.cs
@@ -0,0 +1,4 @@
+﻿double x = 15.8;
+
+Console.WriteLine((int)x);
+Console.WriteLine(Convert.ToInt32(x));
