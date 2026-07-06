a121c8e81baf8e8cd0f8c9c573475049201e1e75
hana zamani <hanazamani96@gmail.com>
2026-07-06 17:34:16 +0330

    add parse practice

---
+++ chapter03/vocabulary42/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("enter a number :");
+int x =int.Parse(Console.ReadLine()!);
+int y=x+5;
+Console.WriteLine(y);
---
+++ chapter03/vocabulary43/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("enter first number:");
+int x=int.Parse(Console.ReadLine()!);
+Console.Write("enter the second number:");
+int y=int.Parse(Console.ReadLine()!);
+int sum = x+y;
+Console.WriteLine($"sum of two numbers is : {sum}");
