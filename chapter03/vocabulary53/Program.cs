adea62b8e5247f4c4860cbecb01598c70c5986dc
hana zamani <hanazamani96@gmail.com>
2026-07-07 17:47:44 +0330

    add exception practice

---
+++ chapter03/vocabulary53/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("enter a number :");
+string number=Console.ReadLine()!;
+try
+{
+    int num=int.Parse(number);
+    Console.WriteLine(num);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ chapter03/vocabulary55/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("enter a number:");
+string number=Console.ReadLine()!;
+try
+{
+    int num=int.Parse(number);
+    Console.WriteLine(num);
+}
+catch (OverflowException)
+{
+    Console.WriteLine("The number is too large.");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Please enter numbers only.");
+
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
