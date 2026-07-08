65d2845ec3b3a26658467e2769c10601bc9268a8
hana zamani <hanazamani96@gmail.com>
2026-07-08 14:42:38 +0330

    add exception practice

---
+++ chapter03/vocabulary56/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("enter a number :");
+string number=Console.ReadLine()!;
+try
+{
+    int num=int.Parse(number);
+    Console.WriteLine(num);
+}
+catch(FormatException) when (number.Contains("$"))
+{
+    Console.WriteLine(" dot use $ .");
+}
+catch(FormatException)
+{
+    Console.WriteLine("enter number.");
+}
