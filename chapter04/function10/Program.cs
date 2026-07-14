158d53187755b020a5534e6e451ceed5abf5f41f
hana zamani <hanazamani96@gmail.com>
2026-07-14 19:21:22 +0330

    add function practice

---
+++ chapter04/function10/Program.cs
@@ -0,0 +1,36 @@
+﻿static decimal CalculateTax(decimal x,string countrycode)
+{ 
+    decimal tax;
+    
+    switch (countrycode)
+    {
+        case "FR" :
+       tax=x*0.20m;
+
+        break;
+
+        case "US" :
+         tax=x*0.08m;
+
+        break;
+
+        case "IR" :
+         tax=x*0.09m;
+
+        break;
+
+        default :
+        tax=x*0.05m;
+
+        break;
+            
+    }
+    Console.WriteLine($"tax of this country is :{tax}");
+    return tax;
+   
+}
+Console.Write("enter the price:");
+decimal price=decimal.Parse(Console.ReadLine()!);
+Console.Write("enter your country:");
+string country =Console.ReadLine()!;
+decimal tax= CalculateTax(price,country);
---
+++ chapter04/function9/Program.cs
@@ -0,0 +1,21 @@
+﻿static decimal CalculateDiscount(decimal x , string type)
+{
+    decimal discount;
+    if (type == "VIP" || type=="vip")
+    {
+         discount=x *0.2m;
+        Console.WriteLine( $"discount :{discount}");
+    }
+    else
+    {
+        discount=x *0.05m;
+        Console.WriteLine($"discount :{discount}");
+    }
+
+    return discount;
+}
+Console.Write("Enter price:");
+decimal price=decimal.Parse(Console.ReadLine()!);
+Console.Write("VIP or normal?");
+string type=Console.ReadLine()!;
+decimal discount= CalculateDiscount(price,type);
