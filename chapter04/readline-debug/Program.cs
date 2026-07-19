f051dc61ecfd178cbf061724a934d71fd6d169e2
hana zamani <hanazamani96@gmail.com>
2026-07-19 19:54:35 +0330

    add chapter 4 practice

---
+++ chapter04/readline-debug/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("enter the price:");
+decimal price=decimal.Parse(Console.ReadLine()!);
+Console.WriteLine("enter the quantity:");
+int quantity =int.Parse(Console.ReadLine()!);
+
+static decimal TotalPrice(decimal price,int quantity)
+{
+
+    
+   if(price<0 || quantity < 0)
+    {
+        
+    
+    throw new ArgumentException("just use positive number for price and quantity");
+    }
+
+    return price*quantity; 
+    
+    
+}
+try {
+Console.WriteLine($"total: {TotalPrice(price,quantity)}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
---
+++ chapter04/readline-debug2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter first number: ");
+
+double firstNumber = double.Parse(Console.ReadLine()!);
+
+
+Console.Write("Enter second number: ");
+
+double secondNumber = double.Parse(Console.ReadLine()!);
+
+
+double result = firstNumber / secondNumber;
+
+
+Console.WriteLine($"Result = {result}");
